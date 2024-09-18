943219f743795a1f13fa5f2321243c03e04be779
Abdelwahab Elghandour <a.elghandour@cequens.com>
2024-09-18 10:58:11 +0300

    add env file

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 // Import the express module
 const express = require("express");
 
+// Import the dotenv module to manage environment variables
+const dotenv = require("dotenv");
+
+// Load environment variables from a .env file into process.env
+dotenv.config({ path: "config.env" });
+
 // Create an instance of an Express application
 const app = express();
 
@@ -10,9 +16,11 @@ app.get("/", (req, res) => {
     res.send("First App");
 });
 
-// Start the server and listen on port 8000
-app.listen(8000, () => {
-    // Log a message to the console when the server starts
-    console.log("First App");
-});
+// Get the port number from environment variables or use 3000 as a default
+const PORT = process.env.PORT || 3000;
 
+// Start the server and listen on the specified port
+app.listen(PORT, () => {
+    // Log a message to the console when the server starts
+    console.log(`First App running on port ${PORT}`);
+});
